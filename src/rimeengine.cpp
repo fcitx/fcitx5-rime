92dab542781fc28ee313b44d0ecdec558c4dc817
Rocka <i@rocka.me>
2023-10-21 18:56:04 +0800

    Call refreshStatusArea in updateConfig.

--- src/rimeengine.cpp
+++ src/rimeengine.cpp
@@ -468,6 +468,7 @@ void RimeEngine::updateConfig() {
     instance_->inputContextManager().registerProperty("rimeState", &factory_);
     updateSchemaMenu();
     refreshSessionPoolPolicy();
+    refreshStatusArea(0);
 }
 
 void RimeEngine::refreshStatusArea(InputContext &ic) {
